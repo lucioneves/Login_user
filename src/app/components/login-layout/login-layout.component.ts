f3d958a8cccd6505d4cb13ab747a7f2af0db3126
Lucio neves <luciocecilia20@gmail.com>
2025-02-11 12:39:56 -0400

    feat: creating initial struct

---
+++ src/app/components/login-layout/login-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-layout',
+  imports: [],
+  templateUrl: './login-layout.component.html',
+  styleUrl: './login-layout.component.scss',
+ 
+
+})
+export class LoginLayoutComponent {
+
+}
